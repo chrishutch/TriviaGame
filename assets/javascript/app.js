4a7def2bce0cd1c336a7668f469a1259d06df5c8
Chris Hutchinson <chrisbond19@me.com>
2017-12-28 22:42:09 -0600

    Added questions and answerswith some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,26 @@ $(document).ready(function() {
 	var intervalId;
 	var correctAnswer;
 	var wrongAnswer;
-	var answers;
-	var question;
+	var answers = {
+        a1: ["Mercury", "Pluto", "Jupiter", "Mars"],
+        a2: ["7 billion years ago", "13.8 billion years ago", "800 billion years ago", "1.2 trillion years ago"],
+        a3: ["1 minute", "8 minutes", "Instantaneous", "24 hours"],
+        a4: ["White dwarf", "Red Giant", "Neutron star", "White hole"],
+        a5: ["Mily Galaxy", "Pinwheel Galaxy", "Milky Way Galaxy", "Andromeda Galaxy"]
+      };
+	var questions = {
+        q1: ["What's the closest planet to us?"],
+        q2: ["How old is the universe?"],
+        q3: ["How long does it take for light to reach the Earth?"],
+        q4: ["Which of these can be formed in a supernova?"],
+        q5: ["What is the name of the galaxy we live in?"]
+      };
+      // Variable to hold the index of current question and answer.
+      var questionIndex = 0;
+      var answerIndex = 0;
+      // Array of questions and answers
+      var questionsArray = [questions.q1, questions.q2, questions.q3, questions.q4, questions.q5];
+      var answersArray = [answers.a1, answers.a2, answers.a3, answers.a4, answers.a5];
 
 	//Start game on click
 	$('#startButton').click(function(){
@@ -21,29 +39,44 @@ $(document).ready(function() {
 		startQuestion();
 	};
 	function startQuestion(){
-		//Question 1
-		question = "What's the closest star to us?";
-		console.log(question);
-		$('#start').html('<h2>'+question+'</h2>');
+		//Start the timer
 		time();
-
+		//Question 1
+      	$('#start').html('<h2>'+questionsArray[questionIndex][0]+'</h2>');
+      	logArray(answersArray[answerIndex]);
+    	//$('#theAnswers').html(answersArray[answerIndex]);
 	};
+	$('#next').click(function () {
+		questionIndex++;
+		answerIndex++;
+		startQuestion();
+
+	});
+	//Use this function to print the answers
+	function logArray(list) {
+        for (var i = 0; i < list.length; i++) {
+          $('#theAnswers').append(list[i]+'<br>');
+          console.log(list[i]);
+        }
+      }
+     //This functions starts the timer
 	function time(){
 		clearInterval(intervalId);
       	intervalId = setInterval(decrement, 1000);
 	}
+	//This functions decreases the timer and stops it
     function decrement() {
-      //  Decrease number by one.
+      //  Decrease timer by one.
       timer--;
-      //  Show the number in the #show-number tag.
+      //Display the time remaining
       $("#display").html("<h2>Time Remaining: " + timer + "</h2>");
       //  Once number hits zero...
       if (timer === 0) {
-        //  ...run the stop function.
-       // startQuestion();
+        //This stops the function.
         clearInterval(intervalId);
-        //  Alert the user that time is up.
+        //Alert the user that time is up.
         console.log("Time Up!");
+        $("#display").html("<h2>Time's Up!</h2>");
       }
     }
 
