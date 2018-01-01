ad17ed9f33ec76a4b20af898ce546073b65d3da8
Chris Hutchinson <chrisbond19@me.com>
2018-01-01 01:21:32 -0600

    Added images after choosinng an answer, also removed some debugging code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,16 @@ $(document).ready(function() {
 	//Variables for the questions, answers, and timer
 	var timer;
 	var intervalId;
-	var correctAnswer = ["Mars", "13.8 billion years ago", "8 minutes", "Neutron star", "Milky Way Galaxy"];
+	var correctAnswer = ["Mars", "13.8 billion years old", "8 minutes", "Neutron star", "Milky Way Galaxy"];
+	//This array holds the correct answer images
+	var correctImage = ["0.gif", "1.gif", "2.gif", "3.gif", "4.gif"];
 	//User's scores
 	var wrongAnswer = 0;
 	var unanswered = 0;
 	var score = 0;
 	var answers = {
         a1: ["Mercury", "Pluto", "Jupiter", "Mars"],
-        a2: ["7 billion years ago", "13.8 billion years ago", "800 billion years ago", "1.2 trillion years ago"],
+        a2: ["7 billion years old", "13.8 billion years old", "800 billion years old", "1.2 trillion years old"],
         a3: ["1 minute", "8 minutes", "Instantaneous", "24 hours"],
         a4: ["White dwarf", "Red Giant", "Neutron star", "White hole"],
         a5: ["Mily Galaxy", "Pinwheel Galaxy", "Milky Way Galaxy", "Andromeda Galaxy"]
@@ -28,7 +30,9 @@ $(document).ready(function() {
       var questionsArray = [questions.q1, questions.q2, questions.q3, questions.q4, questions.q5];
       var answersArray = [answers.a1, answers.a2, answers.a3, answers.a4, answers.a5];
       var gameBegin;
+      //Hides the reset button when the window reloads
       $('#reset').hide();
+
 	//Start game on click
 	$('#startButton').click(function(){
 		//Start game function
@@ -50,8 +54,6 @@ $(document).ready(function() {
 		//This asks and displays the questions
       	$('#start').html('<h2>'+questionsArray[questionIndex][0]+'</h2>');
       	logArray(answersArray[answerIndex]);
-      	//Test/Debug
-    	console.log(answersArray[answerIndex]);
 	};
 	//This functions moves to the next question 
 	//and moves to the score card at the end
@@ -62,11 +64,13 @@ $(document).ready(function() {
 			answerIndex++;
 			startQuestion();
 			$("#answer1, #answer2, #answer3, #answer0").show();
+			$('#images').hide();
 		}
 		//On the last question
 		else if (questionIndex === 4){
 			gameBegin = false;
 			stop();
+			$('#images').hide();
 			$("#answer1, #answer2, #answer3, #answer0").hide();
 			$('#display').hide();
 			$('#start').html("<h2>Your score is: "+ score+"<br>You guessed wrong: "+wrongAnswer+"<br>You didn't guess at all: "+unanswered+'</h2>');
@@ -78,14 +82,12 @@ $(document).ready(function() {
 		//Grabs the string of the clicked answer
 		var word = "";
 		word = $(this).text();
-		//Test/Debug
-		console.log(word);
 		//Check if the correct answer and prints correct answer
 		if(word == correctAnswer[answerIndex]){
-			console.log('you right');
 			$('#start').html("<h2>You're Right!</h2>");
 			$("#answer1, #answer2, #answer3, #answer0").hide();
-			setTimeout(next, 3000);
+			showImage();
+			setTimeout(next, 4000);
 			stop();
 			score++;
 		}
@@ -93,22 +95,28 @@ $(document).ready(function() {
 		else if (word != correctAnswer[answerIndex]){
 			$('#start').html("<h2>You're Wrong! The correct answer is: "+correctAnswer[answerIndex]+"</h2>");
 			$("#answer1, #answer2, #answer3, #answer0").hide();
+			showImage();
 			stop();
-			console.log('you wrong');
-			setTimeout(next, 3000);
+			setTimeout(next, 4000);
 			wrongAnswer++;
 		}
       });
+	//Functions shows and displays correct image
+	function showImage() {
+		$('#images').show();
+		$('#images').html("<img src='assets/images/"+correctImage[answerIndex]+"'>");
+	};
 	//Reset button action
 	$("#reset").on("click", function(){
+		//Reset game to first question
 		reset();
+		//Hides reset button
 		$('#reset').hide();
 	});
 	//Use this function to print the answers
 	function logArray(list) {
         for (var i = 0; i < list.length; i++) {
           $('#answer'+i).html(list[i]+'<br>');
-          console.log(list[i]);
         }
       }
      //This functions starts the timer
@@ -142,7 +150,7 @@ $(document).ready(function() {
 	}
 	//This functions decreases the timer and stops it
     function decrement() {
-      //  Decrease timer by one.
+      // Decrease timer by one
       timer--;
       //Display the time remaining
       $("#display").html("<h2>Time Remaining: " + timer + "</h2>");
